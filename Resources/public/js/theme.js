113ec67613f0c90bf361b2822d021c800f833c78
Kevin te Raa <k.teraa@recognize.nl>
2015-04-29 17:22:49 +0200

    Refined the profile animation for mobile and added german locale

--- Resources/public/js/theme.js
+++ Resources/public/js/theme.js
@@ -12,16 +12,21 @@ $(document).ready(function(){
  * Makes the profile smaller or bigger depending on the scroll position
  */
 $(document).ready(function(){
-
     var profile_element = $('.topbar .profile');
     var toggleBigAvatar = function(){
         profile_element.toggleClass('big').addClass('disable-scroll');
     };
 
+    var mobile_screen_width = 768;
+    if( window.innerWidth <= mobile_screen_width ){
+        profile_element.addClass('big');
+    }
+
+
     $(window).on('scroll', function( event ){
         if( $(window).scrollTop() > 50 ){
             if( profile_element.hasClass('disable-scroll') == false ){
-                profile_element.removeClass('big');
+                profile_element.removeClass('big').addClass('disable-scroll');
             }
         }
     });
--- Resources/theme/js/profile-animation.js
+++ Resources/theme/js/profile-animation.js
@@ -2,16 +2,21 @@
  * Makes the profile smaller or bigger depending on the scroll position
  */
 $(document).ready(function(){
-
     var profile_element = $('.topbar .profile');
     var toggleBigAvatar = function(){
         profile_element.toggleClass('big').addClass('disable-scroll');
     };
 
+    var mobile_screen_width = 768;
+    if( window.innerWidth <= mobile_screen_width ){
+        profile_element.addClass('big');
+    }
+
+
     $(window).on('scroll', function( event ){
         if( $(window).scrollTop() > 50 ){
             if( profile_element.hasClass('disable-scroll') == false ){
-                profile_element.removeClass('big');
+                profile_element.removeClass('big').addClass('disable-scroll');
             }
         }
     });
