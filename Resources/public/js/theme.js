a72b667586bed69320c31186061bdf055d0a1e2a
Kevin te Raa <k.teraa@recognize.nl>
2015-05-06 15:32:46 +0200

    Fixed the softkeyboard listener - Now doesn't trigger on orientation change

--- Resources/public/js/theme.js
+++ Resources/public/js/theme.js
@@ -37,14 +37,24 @@ $(document).ready(function() {
 
 
     if( $(window).width() <= 768 ){
-
         var startheight = $(window).height();
+        var currentwidth = window.innerWidth;
+
         $(window).on("resize", function( event ){
-            if( $(window).height() < startheight ){
+
+            // We need to make sure orientation changes don't trigger the hiding of the topbar
+            // So we have to keep track of width changes
+            if( $(window).height() < startheight && currentwidth == window.innerWidth ){
                 $('.topbar').css('top', '-50px');
             } else {
                 $('.topbar').css('top', '0px');
             }
+
+            if( currentwidth == window.innerWidth ){
+                startheight = $(window).height();
+            }
+
+            currentwidth = window.innerWidth;
         });
     }
 
