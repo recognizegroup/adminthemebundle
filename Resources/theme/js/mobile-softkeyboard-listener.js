cf1bf0905b254a49d88195e8facf22a8ccd28df3
Kevin te Raa <k.teraa@recognize.nl>
2015-05-07 10:07:05 +0200

    Fixed a bug on the topbar hiding on mobile: The urlbar on top sliding up and down on scrolling would cause the hiding and showing to trigger, this has been removed

--- Resources/theme/js/mobile-softkeyboard-listener.js
+++ Resources/theme/js/mobile-softkeyboard-listener.js
@@ -10,12 +10,17 @@ $(document).ready(function() {
 
         $(window).on("resize", function( event ){
 
-            // We need to make sure orientation changes don't trigger the hiding of the topbar
-            // So we have to keep track of width changes
-            if( $(window).height() < startheight && currentwidth == window.innerWidth ){
-                $('.topbar').css('top', '-50px');
-            } else {
-                $('.topbar').css('top', '0px');
+            // Make sure having there has to be a big enough difference in the resizing
+            // So that we know it was the keyboard, and not something else
+            if( Math.abs( $(window).height() - startheight) > 100 ){
+
+                // We need to make sure orientation changes don't trigger the hiding of the topbar
+                // So we have to keep track of width changes
+                if( $(window).height() < startheight && currentwidth == window.innerWidth ){
+                    $('.topbar').css('top', '-50px');
+                } else {
+                    $('.topbar').css('top', '0px');
+                }
             }
 
             if( currentwidth == window.innerWidth ){
