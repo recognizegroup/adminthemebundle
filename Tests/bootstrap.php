f517a6b45c4ab6b2fc733bc03220375b8397e531
Kevin te Raa <k.teraa@recognize.nl>
2015-05-26 10:58:30 +0200

    Removed the examples out of the AdminThemeBundle and into the CMSBundle that will be used to present the design

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,21 +1,24 @@
 <?php
 
-function loadAutoLoad($level = 0, $path = __FILE__) {
-    $path = dirname($path);
-    $dependencyfile = $path . '/app/autoload.php';
-    if (!file_exists($dependencyfile)) {
-        if($level < 20){
-            return loadAutoLoad($level + 1, $path);
+// Make sure the bootstrapping process is only run once
+if( defined('IS_TEST_BOOTED') == false ) {
+    define('IS_TEST_BOOTED', true);
+
+    function loadAutoLoad($level = 0, $path = __FILE__) {
+        $path = dirname($path);
+        $dependencyfile = $path . '/app/autoload.php';
+        if (!file_exists($dependencyfile)) {
+            if ($level < 20) {
+                return loadAutoLoad($level + 1, $path);
+            } else {
+                throw new RuntimeException('Autoload not found');
+            }
         } else {
-            throw new RuntimeException('Autoload not found');
+            return $path;
         }
     }
-    else
-    {
-        return $path;
-    }
-}
 
-$rootpath = loadAutoLoad();
-$autoload = require_once( $rootpath . '/app/autoload.php' );
-$_SERVER['KERNEL_DIR'] = $rootpath . '/app/';
+    $rootpath = loadAutoLoad();
+    $autoload = require_once($rootpath . '/app/autoload.php');
+    $_SERVER['KERNEL_DIR'] = $rootpath . '/app/';
+}
