040ceb99c66321e82236de3e1aef9d78ce4d6c43
Kevin te Raa <k.teraa@recognize.nl>
2015-05-28 11:30:15 +0200

    Added switch form type to make it easy to add a switch widget to a form

---
+++ Form/Type/SwitchType.php
@@ -0,0 +1,19 @@
+<?php
+namespace Recognize\AdminThemeBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+class SwitchType extends AbstractType {
+
+    public function getParent(){
+        return "checkbox";
+    }
+
+    public function getName(){
+        return "switch";
+    }
+
+}
