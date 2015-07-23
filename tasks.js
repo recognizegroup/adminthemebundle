cba8909bb4b91b5cff286b843287135113a101f7
Kevin te Raa <k.teraa@recognize.nl>
2015-07-23 10:58:19 +0200

    Fixed an issue where builds would fail because a file was created in the vendor folder

--- tasks.js
+++ tasks.js
@@ -4,40 +4,49 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
 var webfolder = "web/";
-var defaultfolder = "vendor/recognize/adminthemebundle/Recognize/AdminThemeBundle/Resources/";
 
-module.exports = function( gulp ){
+module.exports = function( gulp, folder, sourcefolder ){
+    if( !folder ){
+        folder = "admintheme";
+    }
+
+    if( !sourcefolder ){
+        sourcefolder = "vendor/recognize/adminthemebundle/Recognize/AdminThemeBundle/Resources/";
+    }
+
+    console.log( folder );
+
     gulp.task('default-theme-sass', function() {
-        gulp.src(defaultfolder + "theme/scss/base.standard.scss")
+        gulp.src(sourcefolder + "theme/scss/base.standard.scss")
             .pipe(sass())
             .pipe(concat('theme.css'))
-            .pipe(gulp.dest(defaultfolder + "public/css"));
+            .pipe(gulp.dest(folder + "/css"));
     });
 
     gulp.task('default-theme-js', function() {
-        return gulp.src(defaultfolder + 'theme/js/*.js')
+        return gulp.src(sourcefolder + 'theme/js/*.js')
             .pipe(concat('theme.js'))
-            .pipe(gulp.dest(webfolder + "admintheme/js"));
+            .pipe(gulp.dest(webfolder + folder + "/js"));
     });
 
     gulp.task('default-theme-move-css', function(){
-        return gulp.src(defaultfolder + "public/css/*")
-            .pipe(gulp.dest(webfolder + "admintheme/css"));
+        return gulp.src(sourcefolder + "public/css/*")
+            .pipe(gulp.dest(webfolder + folder + "/css"));
     });
 
     gulp.task('default-theme-move-js', function(){
-        return gulp.src(defaultfolder + "public/js/*")
-            .pipe(gulp.dest(webfolder + "admintheme/js"));
+        return gulp.src(sourcefolder + "public/js/*")
+            .pipe(gulp.dest(webfolder +  folder + "/js"));
     });
 
     gulp.task('default-theme-move-images', function(){
-        return gulp.src(defaultfolder + "theme/images/default/*")
-            .pipe(gulp.dest(webfolder + "admintheme/images"));
+        return gulp.src(sourcefolder + "theme/images/default/*")
+            .pipe(gulp.dest(webfolder +  folder + "/images"));
     });
 
     gulp.task('default-theme-move-vendors', function(){
-        return gulp.src(defaultfolder + "public/vendor/*/*")
-            .pipe(gulp.dest(webfolder + "admintheme"));
+        return gulp.src(sourcefolder + "public/vendor/*/*")
+            .pipe(gulp.dest(webfolder +  folder));
     });
 
     gulp.task('default-theme-move', ['default-theme-move-vendors', 'default-theme-move-css', 'default-theme-move-js', 'default-theme-move-images']);
